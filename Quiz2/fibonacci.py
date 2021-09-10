de85347f31154db04dd65a361aedf3d123f45607
bwmodlin <82414516+bwmodlin@users.noreply.github.com>
2021-09-10 15:50:36 -0600

    made it easier to read

--- Quiz2/fibonacci.py
+++ Quiz2/fibonacci.py
@@ -24,6 +24,7 @@ fib_nums = np.array([0, 1])
 def get_fib(size):
     global fib_nums
     if (size <= fib_nums.size):
+        print("Fibonacci sequence array: \n")
         print(fib_nums[:(size)])
 
     else:
@@ -36,11 +37,11 @@ get_fib(get_input())
 
 
 if (fib_nums.size > 1):
-    print("approximating golden ratio")
     ratio_nums = np.array([])
     for num in range(2, fib_nums.size-1):
         new_ratio = fib_nums[num] / fib_nums[num-1]
         ratio_nums = np.append(ratio_nums, new_ratio)
-
+    print("Approximating golden ratio array: \n")
+    print(ratio_nums)
     plt.plot(ratio_nums)
     plt.show()
