da14089e6c098c4cc6a9cf98b2b60ee42227c69c
bwmodlin <82414516+bwmodlin@users.noreply.github.com>
2021-09-10 10:26:16 -0600

    graphed data, best fit line

--- Quiz1/readdata.py
+++ Quiz1/readdata.py
@@ -2,3 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 C02_ppm_MaunaLoa = np.load("data.npy")
+
+num_years = (C02_ppm_MaunaLoa.size * 14) / 365
+
+years = np.linspace(1981, 1981+num_years, C02_ppm_MaunaLoa.size)
+
+plt.plot(years, C02_ppm_MaunaLoa)
+
+best_fit = np.polyfit(years, C02_ppm_MaunaLoa, 1)
+
+fit_func = np.poly1d(best_fit)
+
+plt.plot(years, fit_func(years))
+
+plt.show()
