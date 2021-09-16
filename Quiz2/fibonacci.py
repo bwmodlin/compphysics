0643adc8aced9f9c180ff4bef1394729f622a7b1
bwmodlin <82414516+bwmodlin@users.noreply.github.com>
2021-09-16 16:23:20 -0600

    added arraymath

--- Quiz2/fibonacci.py
+++ Quiz2/fibonacci.py
@@ -37,7 +37,8 @@ def get_fib(size):
 get_fib(get_input())
 
 
-if (fib_nums.size >= 3):
+def loopMath():
+    global fib_nums
     ratio_nums = np.array([])
     for num in range(2, fib_nums.size):
         new_ratio = fib_nums[num] / fib_nums[num-1]
@@ -46,11 +47,34 @@ if (fib_nums.size >= 3):
     print("Approximating golden ratio array: ")
     print(ratio_nums)
     print("\n")
+    return ratio_nums
 
+
+def arrayMath():
+    global fib_nums
+    ratio_nums = np.array([])
+
+    fib_nums_rolled = np.roll(fib_nums, 1)
+    ratio_nums = fib_nums[2:] / fib_nums_rolled[2:]
+    print("Approximating golden ratio array: ")
+    print(ratio_nums)
+    print("\n")
+    return ratio_nums
+
+
+def graph_ratios(ratio_nums):
     n_values = np.arange(3, fib_nums.size+1)
+    #ratio_nums = np.abs(ratio_nums - 1.61803398875)
+    #plt.semilogy(n_values, ratio_nums, base=10)
     plt.plot(n_values, ratio_nums)
+
     plt.xlabel("Size of fibonacci sequence (n)")
     plt.ylabel("Golden Ratio Estimate")
     plt.title("Estimating Golden Ratio over the Fibonacci Sequence")
     # plt.savefig("11numberestimation")
     plt.show()
+
+
+if (fib_nums.size >= 3):
+    graph_ratios(arrayMath())
+    # graph_ratios(loopMath())
