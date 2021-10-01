818c9faae698fc0e4337a083081ccd0690e141a4
bwmodlin <82414516+bwmodlin@users.noreply.github.com>
2021-09-30 23:54:57 -0600

    first attempt, shape not working

--- Quiz3/trajectory.py
+++ Quiz3/trajectory.py
@@ -4,8 +4,15 @@ import h5py
 # import the right axis class to give us the 3D option
 from mpl_toolkits.mplot3d import Axes3D
 
+h5_file = h5py.File("test_ssx_particle_trajectory.h5", 'r')
+position_array = h5_file['r'][()]
+
+xarray, yarray, zarray = np.split(position_array, 3, 1)
+
 
 # create a 3D axis object
 ax = plt.gcf().add_subplot(111, projection='3d')
 # make a plot of a trajectory given x, y, and z arrays as a function of time
-ax.plot(xarray, yarray, zarray, )
+ax.plot(xarray, yarray, zarray)
+
+plt.show()
