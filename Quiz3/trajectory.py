8c49de4c17829e7f795c36f8af3c627f3479e93d
bwmodlin <82414516+bwmodlin@users.noreply.github.com>
2021-10-01 00:17:14 -0600

    added photo, put data and code in same folder

--- Quiz3/trajectory.py
+++ Quiz3/trajectory.py
@@ -4,13 +4,15 @@ import h5py
 # import the right axis class to give us the 3D option
 from mpl_toolkits.mplot3d import Axes3D
 
-h5_file = h5py.File("test_ssx_particle_trajectory.h5", 'r')
+h5_file = h5py.File("Quiz3/test_ssx_particle_trajectory.h5", 'r')
 position_array = h5_file['r'][()]
 
 xarray, yarray, zarray = np.split(position_array, 3, 1)
 x1d = np.array([])
 y1d = np.array([])
 z1d = np.array([])
+
+# getting rid of almost empty second dimension to plot (not sure why it is there)
 for i in range(10000):
     x1d = np.append(x1d, xarray[i][0])
     y1d = np.append(y1d, yarray[i][0])
@@ -26,5 +28,7 @@ ax.set_title("3d Trajectory Plot")
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
+
+# plt.savefig("trajectoryplot.jpg")
 
 plt.show()
