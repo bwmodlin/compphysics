cb75296c5c2d70cde181d671e061d539bc266399
bwmodlin <82414516+bwmodlin@users.noreply.github.com>
2021-10-07 13:17:20 -0600

    initial commit: not working yet

---
+++ Quiz4/schrodinger.py
@@ -0,0 +1,39 @@
+import numpy as np
+import scipy.constants as const
+import matplotlib.pyplot as plt
+from scipy.optimize import fsolve
+
+
+k_squared = 2 * const.electron_mass * \
+    (0.529 * (10**(-10))) * (10**3) * const.electron_volt / const.hbar
+k = np.sqrt(k_squared)
+
+print(k)
+
+
+def s_equation(s):
+    eq = s * (1/np.tan(s)) + (np.sqrt(k**2 - s**2))
+    return eq
+
+# (s^2 / k^2) - 1 = - E / V0
+# - V0 (s^2 / k^2) + V0 = E
+
+
+def e_equation(s):
+    return - 1 * 10 ** 3 * (s**2/k**2) + (10**3)
+
+
+print(fsolve(s_equation, 3))
+
+s = np.linspace(-2, 2, 1000)
+y = s_equation(s)
+z = np.zeros(s.shape)
+
+print(y)
+
+plt.figure(1)
+plt.clf()
+plt.plot(s, y, 'r--')
+plt.xlabel('x')
+plt.ylabel('cos(x)-exp(-x)')
+plt.show()
