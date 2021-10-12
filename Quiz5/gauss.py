d9435a9a5ca13b1efcb699f70c11bf7101235bc5
bwmodlin <82414516+bwmodlin@users.noreply.github.com>
2021-10-12 11:15:52 -0600

    got probability function done

--- Quiz5/gauss.py
+++ Quiz5/gauss.py
@@ -1,6 +1,5 @@
 from pylab import *
 from scipy.integrate import quad
-import matplotlib.pyplot as plt
 import numpy as np
 import scipy.constants as const
 
@@ -9,4 +8,15 @@ mean = 0
 
 
 def gauss(x):
-    return (1 / (stddev * np.sqrt(2 * const.pi))) * np.exp((-1/2)*(((x-mean)**2)/stddev))
+    return (1 / (stddev * np.sqrt(2 * const.pi))) * np.exp((-1/2)*(((x-mean)**2)/stddev**2))
+
+
+def getProbability(low, high):
+    integral = quad(gauss, low, high)
+    print("Probability: {0:.5f}%".format(integral[0]*100))
+
+
+x_low = 0
+x_high = 10
+
+getProbability(x_low, x_high)
