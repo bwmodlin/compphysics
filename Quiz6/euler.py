26f9c0f5a2536c5e39ea3c7a90df9e1441295e2f
Benjamin Modlin <b_modlin@coloradocollege.edu>
2021-10-27 10:38:14 -0600

    Added phase portraits and some flexibility

--- Quiz6/euler.py
+++ Quiz6/euler.py
@@ -1,31 +1,75 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x0 = 1.0
-v0 = 0.0
-tau = 0.1
-w = 0.5
 
-x = x0
-v = v0
-t = 0.0
 
-x_plt = np.array([x])
-v_plt = np.array([v])
-t_plt = np.array([t])
+def euler(method):
+    x0 = 1.0
+    v0 = 0.0
+    tau = 1.0
+    w = 0.5
 
-while (t < 50):
-    x = x + v*tau
-    a = -w**2 * x
-    v = v + a*tau
-    t = t + tau
+    x = x0
+    v = v0
+    t = 0.0
 
-    x_plt = np.append(x_plt, x)
-    v_plt = np.append(v_plt, v)
-    t_plt = np.append(t_plt, t)
+    x_plt = np.array([x])
+    v_plt = np.array([v])
+    t_plt = np.array([t])
 
-plt.plot(t_plt, x_plt)
-plt.show()
+    if (method == "Euler"):
+        while (t < w * 100):
+            x = x + v*tau
+            a = -w**2 * x
+            v = v + a*tau
+            t = t + tau
+
+            x_plt = np.append(x_plt, x)
+            v_plt = np.append(v_plt, v)
+            t_plt = np.append(t_plt, t)
+    
+    elif (method == "Comer"):
+        while (t < 50):
+            a = -w**2 * x
+            v = v + a*tau
+            x = x + v*tau
+            t = t + tau
+
+            x_plt = np.append(x_plt, x)
+            v_plt = np.append(v_plt, v)
+            t_plt = np.append(t_plt, t)
+    else:
+        print("Not a valid method")
+        return
+
+    print(t_plt)
+    print(x_plt)
+
+
+    plt.figure(1)
+    plt.plot(t_plt, x_plt)
+    plt.plot(t_plt, v_plt)
+    plt.legend(["Position Graph", "Velocity Graph"])
+    title = "{}'s Method to Approximate Position and Velocity".format(method)
+    plt.xlabel("Time (s)")
+    plt.ylabel("Position (m) / Velocity (m/s)")
+    plt.title(title)
+
+    plt.figure(2)
+    plt.plot(x_plt, v_plt)
+    plt.xlabel("Position (m)")
+    plt.ylabel("Velocity (m/s)")
+    title = "{}'s Method Phase Portrait".format(method)
+    plt.title(title)
+
+    plt.show()
+
+euler("Euler")
+euler("Comer")
+
+
+
+
 
 
 
