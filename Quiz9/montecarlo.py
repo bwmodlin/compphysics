75b9eafe241e58992fb5b1936cd3face0acd9aa9
bwmodlin <b_modlin@coloradocollege.edu>
2021-12-01 13:43:00 -0700

    working one trial at a time now

--- Quiz9/montecarlo.py
+++ Quiz9/montecarlo.py
@@ -12,14 +12,14 @@ in_circle = 0
 
 # (0, 0) at center of square/circle
 for i in range(n_total):
-    guess_x = random.uniform(-radius, radius+1)
-    guess_y = random.uniform(-radius, radius+1)
+    guess_x = random.uniform(-radius, radius)
+    guess_y = random.uniform(-radius, radius)
 
     # x^2 + y^2 = r^2
     if guess_x**2 + guess_y**2 <= radius**2:
         in_circle += 1
 
-# in_circle / n_total = area_of_circle / area_of_square
+
 
 guess_area = (float(in_circle) / float(n_total)) * float(area_of_square)
 
